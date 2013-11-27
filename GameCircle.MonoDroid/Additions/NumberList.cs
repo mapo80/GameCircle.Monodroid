f95287a593e6977f7166c9a6a6e04ab7c59c3da1
Mapo80 <mapo80@gmail.com>
2013-11-27 23:09:12 +0100

    NumberList fix

--- GameCircle.MonoDroid/Additions/NumberList.cs
+++ GameCircle.MonoDroid/Additions/NumberList.cs
@@ -2,11 +2,8 @@ using System;
 
 namespace Com.Amazon.Ags.Client.Whispersync.Model
 {
-	public abstract partial class NumberList : Com.Amazon.Ags.Client.Whispersync.Model.IMergeable
+	public abstract partial class NumberList
 	{
-//		public Java.Lang.Object DeepCopy(){
-//			return base.
-//		}
 		public virtual Java.Lang.Object DeepCopy(){
 			return this.DeepCopy ();
 		}
